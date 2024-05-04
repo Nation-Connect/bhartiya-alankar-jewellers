b5f33e907372d4a9b0f3fe04e70228e06fd101d8
Sugandh Kumar <sugandhkumar9@gmail.com>
2024-05-04 21:47:43 +0530

    Refactor gallery and media pages to use PHP includes for header and footer

--- gallery.php
+++ gallery.php
@@ -1,37 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from mivaanfinejewels.com/gallery.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 May 2024 15:43:59 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
     
     <title>Gallery - Mivaan Fine Jewels</title>
         <!-- <link rel="shortcut icon" href="assets/logo.png" style="background-color:#666"> -->
-  <meta name="keywords" content="Mivaan,Mivaan Fine Jewels,Gold, Silver, Bronze, Antique jewellery, Silver Jewellery,
-  Diamond Jewellery, Pendats, 98k goid, bracelets, chains, ear rings, marriage rings, wedding rings, wedding jewellery, gifting jewellery, gold savings scheme, low wastage, best price jewellery, bangalore jewel store, ">
-    <link rel="stylesheet" href="https://cdn.usebootstrap.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/lightbox.css">
-    <link rel="stylesheet" href="css/index.css">
+  <?php
+    include 'head.php';
+    ?>
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/lightbox.js"></script>
     <script src="js/index.js"></script>
 </head>
 <body>
     <div class="container-fluid">
-        <header class="logo-header">
-            <a href="index.php">
-                <img src="assets/logo.png" alt="This is alt">
-            </a>    
-        </header>
-
-        <nav class="navigation">
-            <a href="about.php">About Us</a>
-            <!-- <a href="media.php">Media</a> -->
-            <a href="gallery.php" class="active">Gallery</a>
-         
-            <a href="contact.php">Contact Us</a>
-        </nav>
+    <?php
+    include 'header.php';
+    ?>
 
         <section class="page-heading">
             <nav aria-label="breadcrumb">
@@ -65,29 +50,12 @@
             </div>
         </section>
 
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 footer-nav">
-                        <img src="assets/logo.png" alt="mivaan logo" width="150">
-                        <ul>
-                            <li><a href="about.php">About</a></li>
-                            <!-- <li><a href="media.php">Media</a></li> -->
-                            <li><a href="gallery.php">Gallery</a></li>
-                            
-                            <li><a href="contact.php">Contact</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-4 footer-address">
-                        <h4>Address:</h4>
-                        <p>Next to lacasa hotel, # 755, 10th Main Rd, 4th Block, Jayanagar, Bengaluru, Karnataka 560011</p>
-                        <p>Phone: 098860 85566</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php
+    include 'footer.php';
+    include 'foot.php';
+    ?>
     </div>
 </body>
 
-<!-- Mirrored from mivaanfinejewels.com/gallery.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 May 2024 15:44:01 GMT -->
+
 </html>
--- media.php
+++ media.php
@@ -6,27 +6,15 @@
 <head>
     
     <title>Media - Mivaan Fine Jewels</title>
-        <meta name="keywords" content="Mivaan,Mivaan Fine Jewels,Gold, Silver, Bronze, Antique jewellery, Silver Jewellery,
-  Diamond Jewellery, Pendats, 98k goid, bracelets, chains, ear rings, marriage rings, wedding rings, wedding jewellery, gifting jewellery, gold savings scheme, low wastage, best price jewellery, bangalore jewel store, ">
-    <link rel="stylesheet" href="https://cdn.usebootstrap.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/index.css">
+        <?php
+    include 'head.php';
+    ?>
 </head>
 <body>
     <div class="container-fluid">
-        <header class="logo-header">
-            <a href="index.php">
-                <img src="assets/logo.png" alt="This is alt">
-            </a>    
-        </header>
-
-        <nav class="navigation">
-            <a href="about.php">About Us</a>
-            <a href="media.php" class="active">Media</a>
-            <a href="gallery.php">Gallery</a>
-      
-            <a href="contact.php">Contact Us</a>
-        </nav>
+    <?php
+    include 'header.php';
+    ?>
 
         <section class="page-heading">
             <nav aria-label="breadcrumb">
@@ -69,30 +57,10 @@
 
         </section>
 
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 footer-nav">
-                        <img src="assets/logo.png" alt="mivaan logo" width="150">
-                        <ul>
-                            <li><a href="about.php">About</a></li>
-                            <li><a href="media.php">Media</a></li>
-                            <li><a href="gallery.php">Gallery</a></li>
-                           
-                            <li><a href="contact.php">Contact</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-4 footer-address">
-                        <h4>Address:</h4>
-                        <p>Next to lacasa hotel, # 755, 10th Main Rd, 4th Block, Jayanagar, Bengaluru, Karnataka 560011</p>
-                        <p>Phone: 098860 85566</p>
-                    </div>
-                </div>
-                <div class="row">
-    
-                </div>
-            </div>
-        </footer>
+        <?php
+    include 'footer.php';
+    include 'foot.php';
+    ?>
     </div>
 </body>
 
